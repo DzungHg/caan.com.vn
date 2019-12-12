b2a57b5bcb0cdba40f269b0bc1ea1694fcb2aaf2
DzungHg <hg.dzung@gmail.com>
2019-12-12 14:30:07 +0700

    trang chủ tiếp
    Còn mục dưới cùng

--- Templates/blocks/_home-slideshow.php
+++ Templates/blocks/_home-slideshow.php
@@ -1,44 +1,32 @@
 <div class="uk-position-relative uk-visible-toggle in-slideshow">
                         <ul class="uk-slideshow-items">
-                            <li>                    
-                                <div class="uk-width-1-2 uk-visible@s uk-cover-container uk-height-1-1">
-                                    <img src="<?=$urls->templates?>styles/images/agivee-slideshow-image1.jpg" data-src="<?=$urls->templates?>styles/images/agivee-slideshow-image1.jpg" alt="image-slide1" width="550" height="420" data-uk-img data-uk-cover>
-                                </div>
-                                <div class="uk-position-center-right uk-width-1-1 uk-width-1-2@s">                        
-                                    <div class="uk-light uk-padding">
-                                        <h3 class="uk-margin-remove-bottom">The new better way to</h3>
-                                        <h1 class="uk-margin-remove-top">Manage business</h1>
-                                        <p class="uk-text-lead uk-visible@m">Software with all the time-saving tools you need to grow your business.</p>
-                                        <a href="#" class="uk-button uk-button-secondary">Get started <span class="uk-margin-small-left" data-uk-icon="icon: fa-arrow-right; ratio:0.028"></span></a>
+                        <?php
+                        $url_image = '';
+                        foreach($page->field_repeater_matrix as $item) {
+                            if($item->type == 'home_slide_show') {
+                                if ($item->images->count())
+                                {
+                                    $url_image = $item->images->first->url();
+                                };
+                                echo "
+                                <li>                    
+                                    <div class='uk-width-1-2 uk-visible@s uk-cover-container uk-height-1-1'>
+                                        <img src='$url_image' data-src='$url_image' alt='image-slide' width='550' height='420' data-uk-img data-uk-cover>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="uk-width-1-2 uk-visible@s uk-cover-container uk-height-1-1">                    
-                                    <img src="<?=$urls->templates?>styles/images/agivee-slideshow-image2.jpg" data-src="<?=$urls->templates?>styles/images/agivee-slideshow-image2.jpg" alt="image-slide2" width="550" height="420" data-uk-img data-uk-cover>
-                                </div>
-                                <div class="uk-position-center-right uk-width-1-1 uk-width-1-2@s">
-                                    <div class="uk-light uk-padding">
-                                        <h3 class="uk-margin-remove-bottom">Professional platform</h3>
-                                        <h1 class="uk-margin-remove-top">For small business</h1>
-                                        <p class="uk-text-lead uk-visible@m">Comprehensive onboarding programs for improving your new business.</p>
-                                        <a href="#" class="uk-button uk-button-secondary">Get started <span class="uk-margin-small-left" data-uk-icon="icon: fa-arrow-right; ratio:0.028"></span></a>
+                                    <div class='uk-position-center-right uk-width-1-1 uk-width-1-2@s'>                        
+                                        <div class='uk-light uk-padding'>
+                                            <h3 class='uk-margin-remove-bottom'>$item->head_line_01</h3>
+                                            <h1 class='uk-margin-remove-top'>$item->head_line_02</h1>
+                                            <p class='uk-text-lead uk-visible@m'>$item->text_area_01</p>
+                                            <a href='$item->link_url' class='uk-button uk-button-secondary'>$item->button_caption <span class='uk-margin-small-left' data-uk-icon='icon: fa-arrow-right; ratio:0.028'></span></a>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="uk-width-1-2 uk-visible@s uk-cover-container uk-height-1-1">
-                                    <img src="<?=$urls->templates?>styles/images/agivee-slideshow-image3.jpg" data-src="<?=$urls->templates?>styles/images/agivee-slideshow-image3.jpg" alt="image-slide3" width="550" height="420" data-uk-img data-uk-cover>
-                                </div>
-                                <div class="uk-position-center-right uk-width-1-1 uk-width-1-2@s">
-                                    <div class="uk-light uk-padding">
-                                        <h3 class="uk-margin-remove-bottom">A real-time view</h3>
-                                        <h1 class="uk-margin-remove-top">Expense reporting</h1>
-                                        <p class="uk-text-lead uk-visible@m">Accurate, real time information helps you make more confident decisions.</p>
-                                        <a href="#" class="uk-button uk-button-secondary">Get started <span class="uk-margin-small-left" data-uk-icon="icon: fa-arrow-right; ratio:0.028"></span></a>
-                                    </div>
-                                </div>
-                            </li>
+                                </li>
+                                ";
+                        
+                            };
+                        }?>
+                          
                         </ul>
                         <div class="uk-slidenav-container uk-flex uk-flex-right">
                             <ul class="uk-slideshow-nav uk-dotnav"></ul>
